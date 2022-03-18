fb43c66ff81d0bf66bc9d8c2c6c03d87ead41199
ivarjon <ivar3145@gmail.com>
2022-03-18 20:04:03 +0000

    Test fyrir orders/:id

---
+++ src/tests/orders.test.js
@@ -0,0 +1,14 @@
+import { test, describe, expect } from '@jest/globals';
+
+import { fetchAndParse } from './utils.js';
+
+describe('orders', () => {
+  test('GET /orders/:id', async () => {
+    const { result, status } = await fetchAndParse('/orders/08626326-a55f-11ec-b909-0242ac120002');
+
+    expect(status).toBe(200);
+    expect(result.price).toBe(80);
+    expect(result.orders).toBeDefined();
+    expect(result.rows).toBeDefined();
+  });
+});
