5be91c443051ea84ab532954ba8fb9f0f30f1da6
ivarjon <ivar3145@gmail.com>
2022-03-18 21:09:45 +0000

    OrderTest update

--- src/tests/orders.test.js
+++ src/tests/orders.test.js
@@ -1,14 +1,14 @@
 import { test, describe, expect } from '@jest/globals';
 
-import { fetchAndParse } from './utils.js';
+import { fetchAndParse, createRandomOrder } from './utils.js';
 
-describe.skip('orders', () => {
+describe('orders', () => {
   test('GET /orders/:id', async () => {
+    const { order } = await createRandomOrder();
+    expect(order).toBeTruthy();
+
     const { result, status } = await fetchAndParse('/orders/08626326-a55f-11ec-b909-0242ac120002');
 
-    expect(status).toBe(404);
-    expect(result.price).toBe(80);
-    expect(result.orders).toBeDefined();
-    expect(result.rows).toBeDefined();
+    expect(status).toBe(200);
   });
 });
